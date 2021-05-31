ed0a43b294cc048bc495029d07cd1cf06f6e63ee
Henrique Naimaier <coubron@yahoo.com.br>
2021-05-31 15:26:24 -0300

    Created Products DAO

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,8 +1,12 @@
-const roteador = require('express').Router()
+const roteador = require('express').Router({ mergeParams: true })
+// Por causa do escopo do express, precisamos usar o mergeParams 
+// para acessar o idFornecedor presente em rotas/fornecedores/index.js
+const Tabela = require('./TabelaProduto')
 
-roteador.get('/', (request, response) => {
+roteador.get('/', async (request, response) => {
+    const produtos = await Tabela.listar(request.params.idFornecedor)
     response.send(
-        JSON.stringify([])
+        JSON.stringify(produtos)
     )
 })
 
